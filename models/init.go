b951249f07895827ffd7fb185f004eea7542c74d
cppbreak <cppbreak@gmail.com>
2014-12-09 21:34:25 +0800

    add convert from mysql to sqlite3

--- models/init.go
+++ models/init.go
@@ -6,6 +6,38 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+/*
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func convert() {
+	// convert
+	orm.RegisterDataBase("from", "mysql", "root:root@/orm_db2?charset=utf8")
+	orm.RegisterDataBase("to", "sqlite3", "./orm_db2.db")
+
+	from := orm.NewOrm()
+	from.Using("from")
+
+	to := orm.NewOrm()
+	to.Using("to")
+
+	List := make([]Post, 0, 100)
+	_, err := from.QueryTable("post").OrderBy("id").All(&List)
+	if err != nil {
+		log.Fatalln("QueryFailed", err)
+	}
+	successNums, err := to.InsertMulti(len(List), List)
+	if err != nil || int(successNums) != len(List) {
+		log.Fatalln("InsertMulti failed", err, successNums, len(List))
+	}
+
+	log.Fatalln("Convert Succ")
+	// end convert
+}
+*/
+
 func init() {
 	target := beego.AppConfig.String("database")
 
